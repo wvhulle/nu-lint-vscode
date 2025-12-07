e713381e4d6c2ad05b20c1b93ec427bcb555dd43
Willem Vanhulle <willemvanhulle@protonmail.com>
2025-12-07 19:36:17 +0100

    Bundle language server

---
+++ esbuild.mjs
@@ -0,0 +1,19 @@
+import * as esbuild from 'esbuild';
+
+const watch = process.argv.includes('--watch');
+
+const config = {
+    entryPoints: ['src/extension.ts'],
+    bundle: true,
+    outfile: 'out/extension.js',
+    external: ['vscode'],
+    format: 'cjs',
+    platform: 'node',
+};
+
+if (watch) {
+    const ctx = await esbuild.context(config);
+    await ctx.watch();
+} else {
+    await esbuild.build(config);
+}
